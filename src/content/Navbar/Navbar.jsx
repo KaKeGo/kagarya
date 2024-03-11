2af918748997d814a2fc5e2c6893052568e0439e
kakego <funfcia@hotmail.com>
2024-03-11 17:17:18 +0100

    Navbar, activepage, show username login

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './Navbar.css'
+
+
 
 const Navbar = () => {
+  const getNavLinkClass = ({ isActive }) => isActive ? 'active__link' : ''
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [username, setUsername] = useState('Username')
+  
   return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/login'>Login</Link>
+    <div className='navbar'>
+
+      <div className='navbar__section logo'>
+        <div className='navbar__logo'>
+          Logo
+        </div>
+      </div>
+
+      <div className='navbar__section links'>
+        <NavLink to='/' end className={getNavLinkClass}>Home</NavLink>
+        <NavLink to='/register' className={getNavLinkClass}>Register</NavLink>
+        <NavLink to='/login' className={getNavLinkClass}>Login</NavLink>
+      </div>
+
+      <div className='navbar__section user'>
+        {isLoggedIn && (
+        <div className='navbar__userprofile'>
+          {username}
+        </div>
+        )}
+      </div>
+
     </div>
   )
 }
