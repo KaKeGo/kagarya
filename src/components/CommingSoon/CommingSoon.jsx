8a37adb4f7a16b8085109a31db0b5c61377e1bdd
kakego <funfcia@hotmail.com>
2024-04-09 22:17:30 +0200

    Add nev comming soon scenerio

--- src/components/CommingSoon/CommingSoon.jsx
+++ src/components/CommingSoon/CommingSoon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import commingSoon from '../../assets/CommingSoon/CommingSoon.gif'
+import commingSoon from '../../assets/CommingSoon/CommingSoon.avif'
 import './CommingSoon.css'
 
 
@@ -8,8 +8,7 @@ import './CommingSoon.css'
 
 const CommingSoon = () => {
   return (
-    <div className='comming__soon__container font-radio-canada'>
-        <p>Comming soon</p>
+    <div className='comming__soon__container font-radio-canada b'>
         <img className='img' src={commingSoon}/>
     </div>
   )
