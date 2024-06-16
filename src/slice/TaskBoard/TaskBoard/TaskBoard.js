18663d1e41a04f1b62e033f287aa16d6e2bf1cb6
kakego <funfcia@hotmail.com>
2024-06-16 21:59:05 +0200

    Create slice for task board endpoint

---
+++ src/slice/TaskBoard/TaskBoard/TaskBoard.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { DEV_URL } from "../../../Api_url";
+
+
+export const taskBoard = createAsyncThunk(
+    'taskBoard/taskBoard',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await axios.get(
+                `${DEV_URL}taskboard/`,
+                { withCredentials: true }
+            )
+            return response.data
+        } catch (err) {
+            return rejectWithValue(err.response.data)
+        }
+    }
+)
+
+const initialState = {
+    task: {},
+    status: 'idle',
+    error: null,
+}
+
+const taskBoardSlice = createSlice({
+    name: 'taskBoard',
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(taskBoard.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(taskBoard.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.task = action.payload
+                state.error = null
+            })
+            addCase(taskBoard.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.payload
+            })
+    }
+})
+
+export default taskBoardSlice.reducer
